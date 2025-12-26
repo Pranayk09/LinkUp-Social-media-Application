15be50914072231f9a2a8535a8e32ee98250e22b
Pranay Kasar <pranaykasar09@gmail.com>
2025-12-26 15:54:34 +0530

    Frontend Updated

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import Chatbox from './pages/Chatbox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import {useUser} from '@clerk/clerk-react'
+import Layout from './pages/Layout'
+import {Toaster} from 'react-hot-toast'
+
+const App = () => {
+
+  const { isSignedIn } = useUser();
+
+  return (
+    <>
+    <Toaster/>
+    <Routes>
+
+      <Route path='/' element={ !isSignedIn ? <Login/> : <Layout/>}>
+      <Route index element={<Feed/>}/>
+      <Route path='messages' element={<Messages/>}/>
+      <Route path='messages/:userId' element={<Chatbox/>}/>
+      <Route path='connections' element={<Connections/>}/>
+      <Route path='discover' element={<Discover/>}/>
+      <Route path='profile' element={<Profile/>}/>
+      <Route path='profile/:profileId' element={<Profile/>}/>
+      <Route path='create-post' element={<CreatePost/>}/>
+      </Route>
+    </Routes>
+    </>
+  )
+
+}
+
+export default App
