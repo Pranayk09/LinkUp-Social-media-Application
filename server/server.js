4ff12e6ac062f3cf1c2c01b7f0b1c85ec7ac4a85
Pranay Kasar <pranaykasar09@gmail.com>
2026-01-01 18:01:19 +0530

    User controllers updated

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 import connectDb from './configs/db.js';
 import { serve } from "inngest/express";
 import { inngest, functions } from './inngest/index.js';
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -11,9 +13,12 @@ await connectDb();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
+
 
 
 app.get('/', (req,res)=> { res.send('Server is Live')});
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/user', userRouter);
 
 app.listen(PORT, ()=> {console.log("Server is Listening on port", PORT)})
